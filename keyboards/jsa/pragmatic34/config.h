39ee8226e626b6c7656e36390c340e2c1f039034
James Sa <james.sa.2013@gmail.com>
2022-05-04 20:25:07 +0800

    fix product id

--- keyboards/jsa/pragmatic34/config.h
+++ keyboards/jsa/pragmatic34/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0F01
+#define PRODUCT_ID   0x0F03
 #define DEVICE_VER   0x0300
 #define MANUFACTURER James Sa
 #define PRODUCT      Pragmatic 34
