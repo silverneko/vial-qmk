f637e2aaf0448a489207847e04184b9c71799ee3
James Sa <james.sa.2013@gmail.com>
2022-08-21 11:41:14 +0800

    update default layout

--- keyboards/jsa/pragmatic60/keymaps/vial/keymap.c
+++ keyboards/jsa/pragmatic60/keymaps/vial/keymap.c
@@ -58,11 +58,11 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [0] = LAYOUT(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PPLS,
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-        KC_LALT, KC_LCTL, KC_LALT, KC_LGUI, LT(3,KC_CAPS), LT(1,KC_BSPC), LT(2,KC_SPC), LT(4,KC_ENT),  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+        KC_RSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_CAPS ,
+        KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, LT(3,KC_CAPS), LT(1,KC_BSPC), LT(2,KC_SPC), LT(4,KC_ENT),  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     [1] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
