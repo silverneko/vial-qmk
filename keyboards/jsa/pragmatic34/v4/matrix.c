4801172ae2bbc7b3e3caf5f95a62c46eb4fe27bb
James Sa <james.sa.2013@gmail.com>
2023-01-17 17:57:29 +0800

    remove direct pin

--- keyboards/jsa/pragmatic34/v4/matrix.c
+++ keyboards/jsa/pragmatic34/v4/matrix.c
@@ -22,35 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "debounce.h"
 #include "quantum.h"
 
-#define ROWS_PER_HAND (MATRIX_ROWS)
-
-
-#ifdef DIRECT_PINS_RIGHT
-#    define SPLIT_MUTABLE
-#else
-#    define SPLIT_MUTABLE const
-#endif
-#ifdef MATRIX_ROW_PINS_RIGHT
-#    define SPLIT_MUTABLE_ROW
-#else
-#    define SPLIT_MUTABLE_ROW const
-#endif
-#ifdef MATRIX_COL_PINS_RIGHT
-#    define SPLIT_MUTABLE_COL
-#else
-#    define SPLIT_MUTABLE_COL const
-#endif
-
-#ifdef DIRECT_PINS
-static SPLIT_MUTABLE pin_t direct_pins[ROWS_PER_HAND][MATRIX_COLS] = DIRECT_PINS;
-#elif (DIODE_DIRECTION == ROW2COL) || (DIODE_DIRECTION == COL2ROW)
-#    ifdef MATRIX_ROW_PINS
-static SPLIT_MUTABLE_ROW pin_t row_pins[ROWS_PER_HAND] = MATRIX_ROW_PINS;
-#    endif // MATRIX_ROW_PINS
-#    ifdef MATRIX_COL_PINS
-static SPLIT_MUTABLE_COL pin_t col_pins[MATRIX_COLS]   = MATRIX_COL_PINS;
-#    endif // MATRIX_COL_PINS
-#endif
+static pin_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
+static pin_t col_pins[MATRIX_COLS]   = MATRIX_COL_PINS;
 
 /* matrix state(1:on, 0:off) */
 extern matrix_row_t raw_matrix[MATRIX_ROWS]; // raw values
@@ -92,36 +65,7 @@ static inline uint8_t readMatrixPin(pin_t pin) {
 
 // matrix code
 
-#ifdef DIRECT_PINS
-
-__attribute__((weak)) void matrix_init_pins(void) {
-    for (int row = 0; row < ROWS_PER_HAND; row++) {
-        for (int col = 0; col < MATRIX_COLS; col++) {
-            pin_t pin = direct_pins[row][col];
-            if (pin != NO_PIN) {
-                setPinInputHigh(pin);
-            }
-        }
-    }
-}
-
-__attribute__((weak)) void matrix_read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row) {
-    // Start with a clear matrix row
-    matrix_row_t current_row_value = 0;
-
-    matrix_row_t row_shifter = MATRIX_ROW_SHIFTER;
-    for (uint8_t col_index = 0; col_index < MATRIX_COLS; col_index++, row_shifter <<= 1) {
-        pin_t pin = direct_pins[current_row][col_index];
-        if (pin != NO_PIN) {
-            current_row_value |= readPin(pin) ? 0 : row_shifter;
-        }
-    }
-
-    // Update the matrix
-    current_matrix[current_row] = current_row_value;
-}
-
-#elif defined(DIODE_DIRECTION)
+#if defined(DIODE_DIRECTION)
 #    if defined(MATRIX_ROW_PINS) && defined(MATRIX_COL_PINS)
 #        if (DIODE_DIRECTION == COL2ROW)
 
@@ -146,7 +90,7 @@ static void unselect_row(uint8_t row) {
 }
 
 static void unselect_rows(void) {
-    for (uint8_t x = 0; x < ROWS_PER_HAND; x++) {
+    for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
         unselect_row(x);
     }
 }
@@ -216,7 +160,7 @@ static void unselect_cols(void) {
 
 __attribute__((weak)) void matrix_init_pins(void) {
     unselect_cols();
-    for (uint8_t x = 0; x < ROWS_PER_HAND; x++) {
+    for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
         if (row_pins[x] != NO_PIN) {
             setPinInputHigh_atomic(row_pins[x]);
         }
@@ -233,7 +177,7 @@ __attribute__((weak)) void matrix_read_rows_on_col(matrix_row_t current_matrix[]
     matrix_output_select_delay();
 
     // For each row...
-    for (uint8_t row_index = 0; row_index < ROWS_PER_HAND; row_index++) {
+    for (uint8_t row_index = 0; row_index < MATRIX_ROWS; row_index++) {
         // Check row pin state
         if (readMatrixPin(row_pins[row_index]) == 0) {
             // Pin LO, set col bit
@@ -266,7 +210,7 @@ void matrix_init(void) {
     memset(matrix, 0, sizeof(matrix));
     memset(raw_matrix, 0, sizeof(raw_matrix));
 
-    debounce_init(ROWS_PER_HAND);
+    debounce_init(MATRIX_ROWS);
 
     matrix_init_quantum();
 }
@@ -276,7 +220,7 @@ uint8_t matrix_scan(void) {
 
 #if defined(DIRECT_PINS) || (DIODE_DIRECTION == COL2ROW)
     // Set row, read cols
-    for (uint8_t current_row = 0; current_row < ROWS_PER_HAND; current_row++) {
+    for (uint8_t current_row = 0; current_row < MATRIX_ROWS; current_row++) {
         matrix_read_cols_on_row(curr_matrix, current_row);
     }
 #elif (DIODE_DIRECTION == ROW2COL)
@@ -290,7 +234,7 @@ uint8_t matrix_scan(void) {
     bool changed = memcmp(raw_matrix, curr_matrix, sizeof(curr_matrix)) != 0;
     if (changed) memcpy(raw_matrix, curr_matrix, sizeof(curr_matrix));
 
-    debounce(raw_matrix, matrix, ROWS_PER_HAND, changed);
+    debounce(raw_matrix, matrix, MATRIX_ROWS, changed);
     matrix_scan_quantum();
 
     return (uint8_t)changed;
