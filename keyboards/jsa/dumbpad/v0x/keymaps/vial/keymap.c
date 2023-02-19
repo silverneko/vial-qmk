be9fe30fe69bac9371af84a80dd21b6add05d872
James Sa <james.sa.2013@gmail.com>
2023-02-19 22:14:28 +0800

    Clean up

--- keyboards/jsa/dumbpad/v0x/keymaps/vial/keymap.c
+++ keyboards/jsa/dumbpad/v0x/keymaps/vial/keymap.c
@@ -55,7 +55,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 #if defined(ENCODER_MAP_ENABLE)
-#define NUM_ENCODERS 1
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
     [1] =   { ENCODER_CCW_CW(KC_BRMD, KC_BRMU)  },
