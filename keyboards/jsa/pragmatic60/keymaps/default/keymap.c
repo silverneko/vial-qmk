f293f181848025c303333f131da930a913ea70e3
James Sa <james.sa.2013@gmail.com>
2023-02-19 22:23:19 +0800

    Default layout

--- keyboards/jsa/pragmatic60/keymaps/default/keymap.c
+++ keyboards/jsa/pragmatic60/keymaps/default/keymap.c
@@ -73,7 +73,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //     KC_LALT, KC_LCTL, KC_LSFT, KC_LGUI, NAV,     FNBSPC,  PNSPC,   ADJUST,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     // ),
     [_COLEMAK_HOME_MOD] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,KC_EQL,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
 
         KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
@@ -83,8 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LALT, KC_LCTL, KC_LSFT, KC_LGUI, NAV,     FNBSPC,  PNSPC,   ADJUST,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     [_FN] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, KC_ESC,  SCNSHOT, DIVVY,   SPOTLT,  _______, KC_PPLS, KC_P7,   KC_P8,   KC_P9,   KC_PERC, _______,
         _______, KC_TAB,  CUT,     COPY,    PSTE,    KC_ENT,  KC_MINS, KC_P4,   KC_P5,   KC_P6,   KC_PAST, _______,
         _______, _______, WKSPLF,  MSNCTL,  WKSPRT,  KC_SPC,  KC_DLR,  KC_P1,   KC_P2,   KC_P3,   KC_PSLS, _______,
@@ -92,22 +90,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_PN] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, KC_CIRC, KC_GRV,  KC_TILD, KC_LCBR, KC_RCBR, KC_TAB,  KC_PGUP, KC_UP,   _______, _______, _______,
         _______, KC_EXLM, KC_AT,   KC_HASH, KC_LPRN, KC_RPRN, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  _______,
         _______, KC_BSLS, KC_AMPR, KC_PIPE, KC_LBRC, KC_RBRC, KC_CAPS, KC_PGDN, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, KC_DEL,  XXXXXXX, _______, _______, _______, _______, _______
     ),
-    [_NAV] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, KC_PPLS, _______, _______, _______, _______, _______,
-        _______, _______, _______, REDO,    UNDO,    _______, KC_MINS, KC_PEQL, KC_DQUO, KC_QUOT, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_LABK, KC_RABK, _______, _______,
-        _______, _______, _______, _______, XXXXXXX, _______, KC_UNDS, _______, _______, _______, _______, _______
-    ),
     [_ADJUST] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 
         _______, RESET,   _______, _______, _______, _______, _______, KC_VOLU, KC_BRIU, ZOOMIN,  _______, _______,
@@ -116,7 +104,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, XXXXXXX, _______, _______, _______, _______
     ),
     [_MINECRAFT] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   RESET,   KC_F7,   KC_F8,   COLEMAK, COLEMAKm,KC_EQL,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
 
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
@@ -151,6 +138,6 @@ void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
   debug_enable=true;
   debug_matrix=true;
-  //debug_keyboard=true;
-  //debug_mouse=true;
+  debug_keyboard=true;
+  debug_mouse=true;
 }
