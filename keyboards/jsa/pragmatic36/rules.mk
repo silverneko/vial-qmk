bdc457927f7ba45ee5cdde774c3bfaea19a92ffe
James Sa <james.sa.2013@gmail.com>
2023-03-22 21:32:17 +0800

    36 first release

---
+++ keyboards/jsa/pragmatic36/rules.mk
@@ -0,0 +1,15 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+NKRO_ENABLE	    = yes       # USB Nkey Rollover
+LTO_ENABLE      = yes
+
+CUSTOM_MATRIX = lite        # Custom matrix for "Duplex Matrix"
+SRC += matrix.c
+
+CAPS_WORD_ENABLE = yes
