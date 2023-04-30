9a60057cca51e52fe543ef7097d87de59482f455
James Sa <james.sa.2013@gmail.com>
2023-04-30 15:57:29 +0800

    clean up 34 directory

--- keyboards/jsa/pragmatic34/pragmatic34.c
+++ keyboards/jsa/pragmatic34/pragmatic34.c
@@ -14,25 +14,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "v4.h"
-#include "i2c_master.h"
-#include "mcp23018.h"
-
-void matrix_init_kb(void) {
-    // put your keyboard start-up code here
-    // runs once when the firmware starts up
-    i2c_init();
-
-    matrix_init_user();
-}
-
-void matrix_scan_kb(void) {
-    // put your looping keyboard code here
-    // runs every cycle (a lot)
-    if (mcp23018_reset_required()) {
-        msp23018_init();
-        // torn_set_led(2, 1);
-    }
-
-    matrix_scan_user();
-}
+#include "pragmatic34.h"
