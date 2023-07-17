a73a588d5bb16d5d98c4ebb7275396d13c9ecf71
Chiang, Yi-Yo <5255547+silverneko@users.noreply.github.com>
2023-07-18 01:12:46 +0800

    runner3680: Add 'silverneko' custom VIAL keymap

---
+++ keyboards/omkbd/runner3680/5x7/keymaps/silverneko/config.h
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* Select hand configuration, MASTER_LEFT is default */
+
+#define MASTER_RIGHT
+// #define EE_HANDS
+
+// Baseline VIAL configurations.
+#define VIAL_KEYBOARD_UID {0xF9, 0x85, 0x99, 0x57, 0x97, 0xF0, 0x84, 0x44}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+// Tap left shift twice to activate CAPS_WORD.
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+
+// I have weak fingers so relax the tapping interval back to default.
+#ifdef TAPPING_TERM
+#undef TAPPING_TERM
+#endif
+#define TAPPING_TERM 200
+
+// Declare that we draw 100mA so HHKB USB1.0 hub would drive this.
+#define USB_MAX_POWER_CONSUMPTION 100
+
+// Hold top-left or top-right while plugging the keyboard in to enter the
+// bootloader and wipe the EEPROM.
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
+#define BOOTMAGIC_LITE_ROW_RIGHT 5
+#define BOOTMAGIC_LITE_COLUMN_RIGHT 0
---
+++ keyboards/omkbd/runner3680/5x7/keymaps/silverneko/rules.mk
@@ -0,0 +1,12 @@
+# Baseline VIAL configurations.
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
+
+# Enable LTO to optimize firmware size.
+LTO_ENABLE = yes
+
+QMK_SETTINGS = yes
+NKRO_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+BOOTMAGIC_ENABLE = yes
