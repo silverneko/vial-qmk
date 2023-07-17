7f42dcef63158f274ca490a647a152b59bb3a22c
Chiang, Yi-Yo <5255547+silverneko@users.noreply.github.com>
2023-07-18 01:12:46 +0800

    runner3680: Add 'silverneko' custom VIAL keymap

---
+++ keyboards/omkbd/runner3680/5x7/keymaps/silverneko/rules.mk
@@ -0,0 +1,17 @@
+# Baseline VIAL configurations.
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
+
+# Enable LTO to optimize firmware size.
+LTO_ENABLE = yes
+
+# Disable unused features to reduce more.
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+
+QMK_SETTINGS = yes
+NKRO_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+BOOTMAGIC_ENABLE = yes
