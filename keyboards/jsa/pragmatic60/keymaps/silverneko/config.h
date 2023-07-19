97c09c7d301d536aa5ff87ad4e9f36b4dc8a5cb7
Yo <5255547+silverneko@users.noreply.github.com>
2023-07-20 05:40:24 +0800

    pragmatic60: Enable CAPS_WORD and BOOTMAGIC_LITE

---
+++ keyboards/jsa/pragmatic60/keymaps/silverneko/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x00, 0x93, 0xD0, 0x93, 0x88, 0xD4, 0xDA, 0x47}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+// Tap left shift twice to activate CAPS_WORD.
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
